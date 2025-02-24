d027a4d8447d7d3872369bd180092f79d9e745c3
parthshah29 <parths29@gmail.com>
2025-02-24 00:45:27 -0500

    Changed clear button text and page title
    Clear Button: Clear Chat History
    Page Title: Snowflake.AI

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,8 +3,8 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `Chat with Snowflake`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `© 2025 Customized AI by PS @UNC — BETA Version of Snowflake — Answers may not be fully factual`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat History`;
+export const PAGE_TITLE: string = `Snowflake.AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
